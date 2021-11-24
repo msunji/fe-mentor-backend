ab96eb73388950e669cbdc5e507bfc624dd3e67d
Marge <marge.consunji@gmail.com>
2021-11-24 11:58:03 +0800

    Handle routing

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const connection = require('./db-connection');
+
+app.get('/solutions', (req, res) => {
+  // console.log('Test data handling');
+  connection.getSolutions();
+});
+
 app.listen(PORT, () => { console.log(`app listening at Port ${PORT}`)});
